28c8aed2cb8446ef26f5d00d907c9cd073ef890e
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-08-19 15:53:59 +0300

    Add navBar frontend

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {GlobalStyle} from "./components/GlobalStyle/GlobalStyle";
+import NavBar from "./components/NavBar/NavBar";
+import GifAnimation from "./components/Gif/GifAnimation";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <GlobalStyle/>
+            <NavBar/>
+            <GifAnimation/>
+        </div>
+    );
 }
 
 export default App;
