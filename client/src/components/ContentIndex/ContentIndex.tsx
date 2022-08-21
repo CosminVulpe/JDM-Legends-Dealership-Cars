b1acad2213a3432c7ce7c41ecb4245c4095ded08
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-08-21 13:43:03 +0300

    Add content index page

--- client/src/components/ContentIndex/ContentIndex.tsx
+++ client/src/components/ContentIndex/ContentIndex.tsx
@@ -1,49 +1,97 @@
 import React from "react";
+import styled, {css} from 'styled-components/macro';
+import Image1 from "../../Images/Images-project/image1.jpg";
+import Button from "../Button/Button";
 
+const Section = styled.section`
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+`;
+
+const Container = styled.div`
+  padding: 1rem calc((100vw - 1300px) / 2);
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: 600px;
+  @media screen and (max-width: 768px) {
+    grid-template-columns: 1fr;
+  }
+`;
+
+
+const ColumnLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  line-height: 1.4;
+  padding: 1rem 2rem;
+  //order: 2;
+
+  h1 {
+    margin-bottom: 1rem;
+    font-size: clamp(1.5rem, 6vw, 2rem);
+  }
+
+  p {
+    margin-bottom: 2rem;
+  }
+`;
+
+const ColumnRight = styled.div`
+  padding: 1rem 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  order: 1;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+
+    @media screen and (max-width: 768px) {
+      width: 90%;
+      height: 90%;
+      margin-top: 25px;
+    }
+  }
+`;
 
 const ContentIndex: React.FC = () => {
     return (
         <div id="content-index">
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                dolore magna aliqua. Adipiscing tristique risus nec feugiat in fermentum. Mi in nulla posuere
-                sollicitudin aliquam ultrices. Netus et malesuada fames ac turpis egestas. Leo urna molestie at
-                elementum eu facilisis. Massa massa ultricies mi quis. Vestibulum morbi blandit cursus risus. Massa
-                sapien faucibus et molestie ac feugiat sed. Lobortis scelerisque fermentum dui faucibus in ornare quam
-                viverra. Suspendisse interdum consectetur libero id faucibus.</p>
+            <Section>
+                <Container>
+                    <ColumnLeft>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+                            labore
+                            et
+                            dolore magna aliqua. Adipiscing tristique risus nec feugiat in fermentum. Mi in nulla
+                            posuere
+                            sollicitudin aliquam ultrices. Netus et malesuada fames ac turpis egestas. Leo urna molestie
+                            at
+                            elementum eu facilisis. Massa massa ultricies mi quis. Vestibulum morbi blandit cursus
+                            risus.
+                        </p>
+                        <p>Rhoncus urna neque viverra justo nec ultrices dui. Ultricies mi quis hendrerit dolor. Nec
+                            ullamcorper
+                            sit amet risus nullam eget. Aliquam malesuada bibendum arcu vitae elementum. Urna id
+                            volutpat
+                            lacus
+                            laoreet non. Adipiscing elit duis tristique sollicitudin. Mi proin sed libero enim.</p>
+                        <Button buttonText={"Learn More"}/>
+                    </ColumnLeft>
 
-            <p>Rhoncus urna neque viverra justo nec ultrices dui. Ultricies mi quis hendrerit dolor. Nec ullamcorper
-                sit amet risus nullam eget. Aliquam malesuada bibendum arcu vitae elementum. Urna id volutpat lacus
-                laoreet non. Adipiscing elit duis tristique sollicitudin. Mi proin sed libero enim. Ultrices dui sapien
-                eget mi. Vehicula ipsum a arcu cursus vitae congue mauris. Cras pulvinar mattis nunc sed blandit libero
-                volutpat sed cras. Augue interdum velit euismod in pellentesque.</p>
-
-            <p> Cras adipiscing enim eu turpis egestas pretium. Adipiscing commodo elit at imperdiet dui accumsan. A
-                arcu cursus vitae congue. Pharetra massa massa ultricies mi quis hendrerit dolor. Dolor morbi non arcu
-                risus quis varius quam. Feugiat nisl pretium fusce id. Eu volutpat odio facilisis mauris sit amet massa
-                vitae tortor. Et netus et malesuada fames. Interdum velit laoreet id donec ultrices tincidunt arcu non
-                sodales. In hendrerit gravida rutrum quisque non tellus orci ac. Vitae turpis massa sed elementum. Sit
-                amet est placerat in egestas erat imperdiet. In mollis nunc sed id semper risus in hendrerit gravida.
-                Lacus luctus accumsan tortor posuere ac. Senectus et netus et malesuada fames ac turpis egestas. Quam
-                nulla porttitor massa id neque aliquam vestibulum. Mi sit amet mauris commodo quis.</p>
-
-            <p> Et pharetra pharetra massa massa ultricies mi. Semper eget duis at tellus at urna condimentum. Tempus
-                urna et pharetra pharetra massa massa ultricies. Enim eu turpis egestas pretium aenean. Ut placerat orci
-                nulla pellentesque dignissim enim sit amet. Aliquam etiam erat velit scelerisque in dictum non
-                consectetur. Diam quis enim lobortis scelerisque fermentum dui faucibus in ornare. Nulla aliquet enim
-                tortor at auctor. Molestie at elementum eu facilisis sed odio morbi. Ut sem viverra aliquet eget. Nibh
-                sit amet commodo nulla facilisi. Est ante in nibh mauris. Tempus egestas sed sed risus pretium.
-                Consectetur libero id faucibus nisl tincidunt eget nullam. Venenatis a condimentum vitae sapien
-                pellentesque habitant. Et odio pellentesque diam volutpat commodo sed egestas egestas. Elit duis
-                tristique sollicitudin nibh sit amet.</p>
-
-            <p> Lobortis mattis aliquam faucibus purus in massa tempor nec. Sed tempus urna et pharetra pharetra. Aenean
-                pharetra magna ac placerat. Leo vel fringilla est ullamcorper. Venenatis urna cursus eget nunc
-                scelerisque viverra. A diam sollicitudin tempor id eu nisl nunc mi. Mollis nunc sed id semper risus.
-                Posuere ac ut consequat semper viverra nam libero justo. Nulla pellentesque dignissim enim sit amet
-                venenatis urna. Ultrices neque ornare aenean euismod. Felis eget velit aliquet sagittis id.</p>
+                    <ColumnRight>
+                        <img src={Image1} alt="car-line-up"/>
+                    </ColumnRight>
+                </Container>
+            </Section>
         </div>
-    )
+    );
 }
 
 export default ContentIndex;
