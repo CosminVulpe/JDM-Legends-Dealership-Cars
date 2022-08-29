24d4e4aff22a382ace785a64f023293f51eb9dfe
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-08-29 15:25:44 +0300

    Add car controller backend

--- API/src/main/java/com/example/demo/DemoApplication.java
+++ API/src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,6 @@
 package com.example.demo;
 
 import com.example.demo.model.Car;
-import com.example.demo.model.enums.CarColor;
-import com.example.demo.model.enums.CarCompany;
-import com.example.demo.model.enums.CarFuelType;
-import com.example.demo.model.enums.CarTransmissionType;
 import com.example.demo.service.DAO.CarRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,8 +9,9 @@ import org.springframework.context.annotation.Bean;
 
 import java.util.List;
 
-import static com.example.demo.model.enums.CarColor.BLACK;
-import static com.example.demo.model.enums.CarCompany.NISSAN;
+import static com.example.demo.model.enums.CarColor.*;
+import static com.example.demo.model.enums.CarCompany.*;
+import static com.example.demo.model.enums.CarFuelType.DIESEL;
 import static com.example.demo.model.enums.CarFuelType.GASOLINE;
 import static com.example.demo.model.enums.CarTransmissionType.MANUAL_TRANSMISSION;
 
@@ -39,9 +36,84 @@ public class DemoApplication {
                     .price(68980)
                     .hp(300)
                     .damaged(false)
+                    .productionYear(1998)
                     .build();
 
-            carRepository.save(nissan);
+            Car toyota = Car
+                    .builder()
+                    .carName("Toyota Supra")
+                    .carColor(RED)
+                    .carTransmissionType(MANUAL_TRANSMISSION)
+                    .carCompany(TOYOTA)
+                    .carFuelType(DIESEL)
+                    .km(100000)
+                    .price(44900)
+                    .hp(300)
+                    .damaged(true)
+                    .productionYear(1994)
+                    .build();
+
+            Car honda = Car
+                    .builder()
+                    .carName("Honda S2000")
+                    .carColor(YELLOW)
+                    .carTransmissionType(MANUAL_TRANSMISSION)
+                    .carCompany(HONDA)
+                    .carFuelType(GASOLINE)
+                    .km(89500)
+                    .price(33900)
+                    .hp(300)
+                    .damaged(false)
+                    .productionYear(2005)
+                    .build();
+
+            Car subaru = Car
+                    .builder()
+                    .carName("Subaru Impreza WRX STI")
+                    .carColor(BLUE)
+                    .carTransmissionType(MANUAL_TRANSMISSION)
+                    .carCompany(SUBARU)
+                    .carFuelType(GASOLINE)
+                    .km(11600)
+                    .price(33900)
+                    .hp(300)
+                    .damaged(true)
+                    .productionYear(2005)
+                    .build();
+
+            Car mazda = Car
+                    .builder()
+                    .carName("Mazda RX-7 Type R")
+                    .carColor(WHITE)
+                    .carTransmissionType(MANUAL_TRANSMISSION)
+                    .carCompany(MAZDA)
+                    .carFuelType(GASOLINE)
+                    .km(81600)
+                    .price(29600)
+                    .hp(300)
+                    .damaged(false)
+                    .productionYear(1999)
+                    .build();
+
+            Car mitsubichi = Car
+                    .builder()
+                    .carName("Evolution Evo 9")
+                    .carColor(YELLOW)
+                    .carTransmissionType(MANUAL_TRANSMISSION)
+                    .carCompany(MITSUBISHI)
+                    .carFuelType(DIESEL)
+                    .km(81600)
+                    .price(34000)
+                    .hp(300)
+                    .damaged(false)
+                    .productionYear(2005)
+                    .build();
+
+            carRepository.saveAll(
+                    List.of(
+                            nissan, toyota, honda, subaru, mazda, mitsubichi
+                    ));
+
         };
     }
 }
--- API/src/main/java/com/example/demo/controller/CarController.java
+++ API/src/main/java/com/example/demo/controller/CarController.java
@@ -1,8 +1,10 @@
 package com.example.demo.controller;
 
 import com.example.demo.model.Car;
+import com.example.demo.model.enums.CarCompany;
 import com.example.demo.service.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins="https://localhost:3000/")
-@RequestMapping(path="/car")
+@CrossOrigin(origins = "http://localhost:3000/", allowedHeaders = "*")
+@RequestMapping(path = "/car")
 public class CarController {
     private final CarService carService;
 
@@ -22,8 +24,13 @@ public class CarController {
     }
 
     @GetMapping
-    public List<Car> getAll(){
+    public ResponseEntity<List<Car>> getAll() {
         return carService.getAllCars();
+    }
+
+    @GetMapping(path = "/brand")
+    public ResponseEntity<List<CarCompany>> getBrandCar() {
+        return carService.getCarsBrand();
     }
 
 }
