aa4c6681cc4402d639e4c0cfe88ffe3b538bde6d
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-11-16 14:19:04 +0200

    Add method findCarById backend for one-car-page

--- API/src/main/java/com/example/demo/service/CarService.java
+++ API/src/main/java/com/example/demo/service/CarService.java
@@ -1,14 +1,13 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Car;
-import com.example.demo.model.enums.CarCompany;
 import com.example.demo.service.DAO.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Objects;
+import java.util.Optional;
 
 @Service
 public class CarService {
@@ -25,4 +24,11 @@ public class CarService {
                 ResponseEntity.ok(carRepository.findAll()) : ResponseEntity.notFound().build();
     }
 
+    public ResponseEntity<Car> getCarById(Long carId) {
+        Optional<Car> optionalCar = carRepository.findById(carId);
+        return optionalCar.map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity
+                        .badRequest()
+                        .body(null));
+    }
 }
