08c776edd3d44e926db6a25d8b53e0f67259f391
Vulpe Cosmin <cosmin98vulpe@gmail.com>
2022-11-17 15:30:41 +0200

    Add Review Controller entity

---
+++ API/src/main/java/com/example/demo/controller/ReviewController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Review;
+import com.example.demo.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:3000/", allowedHeaders = "*")
+@RequestMapping(path = "/review")
+public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService) {
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Review> addReview(@RequestBody Review review) {
+        return reviewService.addReview(review);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Review>> getRecentReviews(){
+        return reviewService.getRecentReviews();
+    }
+
+}
