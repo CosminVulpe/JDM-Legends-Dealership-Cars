4aa09160a29079e6833f029d7f23a3a02970b39a
CosminVulpe <cosmin98vulpe@gmail.com>
2023-06-24 17:46:59 +0300

    Add micro-service structure 'users' backend

--- JDM-Legends/Users/src/main/java/com/jdm/legends/users/controller/TemporaryUserController.java
+++ JDM-Legends/Users/src/main/java/com/jdm/legends/users/controller/TemporaryUserController.java
@@ -1,2 +1,21 @@
-package com.jdm.legends.users.controller;public class TemporaryUserController {
+package com.jdm.legends.users.controller;
+
+import com.jdm.legends.users.service.TemporaryUserService;
+import com.jdm.legends.users.service.dto.TemporaryUser;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000/", allowedHeaders = "*")
+@RequestMapping(path = "/temporary-user")
+public class TemporaryUserController {
+    private final TemporaryUserService service;
+
+    @PostMapping
+    public void addTempUser(@RequestBody @Valid TemporaryUser temporaryUser) {
+        service.addTemporaryUser(temporaryUser);
+    }
 }
--- JDM-Legends/Users/src/main/java/com/jdm/legends/users/service/TemporaryUserService.java
+++ JDM-Legends/Users/src/main/java/com/jdm/legends/users/service/TemporaryUserService.java
@@ -1,2 +1,16 @@
-package com.jdm.legends.users.service;public class TemporaryUserService {
+package com.jdm.legends.users.service;
+
+import com.jdm.legends.users.service.dto.TemporaryUser;
+import com.jdm.legends.users.service.repository.TemporaryUserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TemporaryUserService {
+    private final TemporaryUserRepository repository;
+
+    public void addTemporaryUser(TemporaryUser temporaryUser) {
+        repository.save(temporaryUser);
+    }
 }
