af964e7c395135c5f32c59d19e4c20041347b853
Vulpe Cosmin <86559678+CosminVulpe@users.noreply.github.com>
2023-07-02 18:13:59 +0300

    Delete JDM-Legends/Users directory

--- JDM-Legends/Users/src/main/java/com/jdm/legends/users/service/repository/TemporaryUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.jdm.legends.users.service.repository;
-
-import com.jdm.legends.users.service.dto.TemporaryUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TemporaryUserRepository extends JpaRepository<TemporaryUser, Long> {
-}
