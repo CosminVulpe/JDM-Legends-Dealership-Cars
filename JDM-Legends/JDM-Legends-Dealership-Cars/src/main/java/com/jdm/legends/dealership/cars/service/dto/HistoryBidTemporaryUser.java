bc00d90c02a5c0fd970f4c833c84bea6f4954d77
CosminVulpe <cosmin98vulpe@gmail.com>
2023-07-16 18:07:47 +0300

    [Backend] Delete test for HistoryBidService

--- JDM-Legends/JDM-Legends-Dealership-Cars/src/main/java/com/jdm/legends/dealership/cars/service/dto/HistoryBidTemporaryUser.java
+++ JDM-Legends/JDM-Legends-Dealership-Cars/src/main/java/com/jdm/legends/dealership/cars/service/dto/HistoryBidTemporaryUser.java
@@ -3,10 +3,12 @@ package com.jdm.legends.dealership.cars.service.dto;
 import com.jdm.legends.common.dto.HistoryBid;
 import com.jdm.legends.common.dto.TemporaryUser;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
 @Data
+@Builder
 @RequiredArgsConstructor
 @AllArgsConstructor
 public class HistoryBidTemporaryUser {
