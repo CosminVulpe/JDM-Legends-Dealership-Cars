e0947d7d2bae4d2f56f8723feca65f905a0751ec
CosminVulpe <cosmin98vulpe@gmail.com>
2023-07-30 14:12:51 +0300

    [Backend - JDM-Legends-Users] remove unnecessary code for now

--- JDM-Legends/JPA-Legend-Users/src/main/java/com/jdm/legends/users/controller/TemporaryUserController.java
+++ JDM-Legends/JPA-Legend-Users/src/main/java/com/jdm/legends/users/controller/TemporaryUserController.java
@@ -1,7 +1,6 @@
 package com.jdm.legends.users.controller;
 
 import com.jdm.legends.common.dto.TemporaryUser;
-import com.jdm.legends.users.service.repository.TemporaryUserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,10 +11,8 @@ import javax.validation.Valid;
 @CrossOrigin(origins = "http://localhost:3000/", allowedHeaders = "*")
 @RequestMapping(path = "/temporary-user")
 public class TemporaryUserController {
-    private final TemporaryUserRepository repository;
 
     @PostMapping(path = "/save")
     public void saveTempUser(@RequestBody @Valid TemporaryUser temporaryUser){
-        repository.save(temporaryUser);
     }
 }
