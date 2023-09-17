b4178d2523e62e5f42641cd5d1f1036f9b2b553a
CosminVulpe <cosmin98vulpe@gmail.com>
2023-09-17 19:52:30 +0400

    [Backend - JDM-Legends-Dealership-Auto] fix problem - save tempUser in its micro-service

--- JDM-Legends/JDM-Legends-Dealership-Cars/src/main/java/com/jdm/legends/dealership/cars/service/HistoryBidService.java
+++ JDM-Legends/JDM-Legends-Dealership-Cars/src/main/java/com/jdm/legends/dealership/cars/service/HistoryBidService.java
@@ -2,11 +2,10 @@ package com.jdm.legends.dealership.cars.service;
 
 import com.jdm.legends.common.dto.Car;
 import com.jdm.legends.common.dto.HistoryBid;
+import com.jdm.legends.common.dto.HistoryBidTemporaryUser;
 import com.jdm.legends.common.dto.TemporaryUser;
-import com.jdm.legends.dealership.cars.service.dto.HistoryBidTemporaryUser;
 import com.jdm.legends.dealership.cars.service.repository.HistoryBidRepository;
 import com.jdm.legends.dealership.cars.service.repository.TemporaryUserRepo;
-import com.jdm.legends.dealership.cars.service.repository.TemporaryUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -18,7 +17,6 @@ public class HistoryBidService {
     private final CarService carService;
     private final TemporaryUserRepo temporaryUserRepo;
     private final HistoryBidRepository historyBidRepository;
-    private final TemporaryUserRepository temporaryUserRepository;
 
     private static final String POTENTIAL_CLIENT = "Potential Client";
     private static final String ANONYMOUS = "Anonymous";
@@ -29,14 +27,10 @@ public class HistoryBidService {
         TemporaryUser temporaryUser = request.getTemporaryUser();
 
         car.addHistoryBid(historyBid);
-        HistoryBid historyBidSaved = historyBidRepository.save(historyBid);
+        historyBidRepository.save(historyBid);
 
         temporaryUser.setRole((temporaryUser.isCheckInformationStoredTemporarily()) ? POTENTIAL_CLIENT : ANONYMOUS);
-        temporaryUser.addHistoryBid(historyBidSaved);
-
-        temporaryUserRepository.save(temporaryUser);
-
+        temporaryUserRepo.saveTempUser(request);
         log.info("Bid Value saved for the car with ID {}", car.getId());
     }
-
 }
