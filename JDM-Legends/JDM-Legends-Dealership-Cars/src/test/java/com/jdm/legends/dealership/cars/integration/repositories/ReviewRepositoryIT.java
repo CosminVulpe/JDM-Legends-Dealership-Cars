fc9eb5b989b17e277b63289f6af80ddb8efd93ea
CosminVulpe <cosmin98vulpe@gmail.com>
2023-10-22 17:30:20 +0400

    [Backend - JDM-Legends-Dealership-Cars] add integration for Review Repository

---
+++ JDM-Legends/JDM-Legends-Dealership-Cars/src/test/java/com/jdm/legends/dealership/cars/integration/repositories/ReviewRepositoryIT.java
@@ -0,0 +1,35 @@
+package com.jdm.legends.dealership.cars.integration.repositories;
+
+import com.jdm.legends.dealership.cars.service.dto.Review;
+import com.jdm.legends.dealership.cars.service.repository.ReviewRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.IntStream;
+
+import static com.jdm.legends.dealership.cars.utils.UtilsMock.buildReviewRequest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ActiveProfiles("test-in-memory")
+@Transactional
+public class ReviewRepositoryIT {
+    @Autowired
+    private ReviewRepository reviewRepository;
+
+
+    @Test
+    void shouldGetRecentReviews() {
+        IntStream.range(0, 7).forEach(item-> reviewRepository.save(buildReviewRequest()));
+
+        List<Review> recentReviews = reviewRepository.getRecentReviews();
+
+        assertThat(recentReviews).isNotEmpty();
+        assertThat(recentReviews.size()).isEqualTo(5);
+    }
+}
