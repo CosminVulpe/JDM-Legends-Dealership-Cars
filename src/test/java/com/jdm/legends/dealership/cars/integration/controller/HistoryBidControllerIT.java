f2e6ccc0b8d6214b5caf742ac5fa57cf651c5850
CosminVulpe <cosmin98vulpe@gmail.com>
2023-11-26 18:07:33 +0400

    slight refactoring HistoryBidControllerIT

--- src/test/java/com/jdm/legends/dealership/cars/integration/controller/HistoryBidControllerIT.java
+++ src/test/java/com/jdm/legends/dealership/cars/integration/controller/HistoryBidControllerIT.java
@@ -1,10 +1,9 @@
 package com.jdm.legends.dealership.cars.integration.controller;
 
-import com.jdm.legends.dealership.cars.service.dto.Car;
-import com.jdm.legends.dealership.cars.service.dto.HistoryBid;
-import com.jdm.legends.dealership.cars.service.dto.HistoryBidTemporaryUserRequest;
-import com.jdm.legends.dealership.cars.service.entity.TemporaryCustomer;
-import com.jdm.legends.dealership.cars.service.enums.Roles;
+import com.jdm.legends.dealership.cars.controller.dto.HistoryBidRequest;
+import com.jdm.legends.dealership.cars.controller.dto.HistoryBidTemporaryCustomerRequest;
+import com.jdm.legends.dealership.cars.controller.dto.TemporaryCustomerRequest;
+import com.jdm.legends.dealership.cars.service.entity.Car;
 import com.jdm.legends.dealership.cars.service.repository.CarRepository;
 import com.jdm.legends.dealership.cars.service.repository.HistoryBidRepository;
 import com.jdm.legends.dealership.cars.service.repository.TemporaryCustomerRepo;
@@ -19,11 +18,15 @@ import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilde
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.transaction.annotation.Transactional;
 
-import static com.jdm.legends.dealership.cars.utils.TestData.buildCarRequest;
-import static com.jdm.legends.dealership.cars.utils.Utils.writeJsonAsString;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+import static com.jdm.legends.dealership.cars.utils.TestDummy.buildCarRequest;
+import static com.jdm.legends.dealership.cars.utils.UtilsMock.writeJsonAsString;
+import static java.time.LocalDateTime.now;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doNothing;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -45,27 +48,23 @@ class HistoryBidControllerIT {
     @MockBean
     private TemporaryCustomerRepo temporaryCustomerRepo;
 
-
     @Test
     void testBidSuccessfully() throws Exception {
-        Car car = carRepository.save(buildCarRequest());
-        HistoryBid historyBid = car.getHistoryBidList().get(0);
-        TemporaryCustomer temporaryCustomer = historyBid.getTemporaryUsersList().stream().findFirst().orElse(new TemporaryCustomer());
-        HistoryBidTemporaryUserRequest request = HistoryBidTemporaryUserRequest.builder().temporaryUser(temporaryCustomer).historyBid(historyBid).build();
+        Car buildCarRequest = buildCarRequest();
+        buildCarRequest.setHistoryBidList(new ArrayList<>());
+        Car car = carRepository.save(buildCarRequest);
+        HistoryBidRequest historyBidRequest = new HistoryBidRequest(new BigDecimal("23423424"), now().minusHours(5));
+        TemporaryCustomerRequest temporaryCustomerRequest = new TemporaryCustomerRequest("John Smith", "john23", "john23@yahoo.com", "ADMIN", true);
+        HistoryBidTemporaryCustomerRequest request = new HistoryBidTemporaryCustomerRequest(historyBidRequest, temporaryCustomerRequest);
+        doNothing().when(temporaryCustomerRepo).saveTempUser(any(),any());
 
         MockHttpServletRequestBuilder builder = MockMvcRequestBuilders.post("/history-bid/bid/{carId}", car.getId())
                 .contentType(APPLICATION_JSON)
                 .content(writeJsonAsString(request))
                 .accept(APPLICATION_JSON);
 
-        doNothing().when(temporaryCustomerRepo).saveTempUser(any());
-
         mvc.perform(builder).andExpect(status().isOk());
 
-        assertThat(car.getHistoryBidList()).hasSize(2);
         assertThat(historyBidRepository.findAll()).hasSize(1);
-        assertThat(temporaryCustomer.getEmailAddress()).isEqualTo("johnCeva12@yahoo.com");
-        assertThat(temporaryCustomer.getRole()).isEqualTo(Roles.POTENTIAL_CLIENT.getValue());
-        verify(temporaryCustomerRepo, atLeastOnce()).saveTempUser(any());
     }
 }
