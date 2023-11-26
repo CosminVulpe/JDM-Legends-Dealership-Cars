17136a84c565007e2de82a1c15690cf1a4ad7414
CosminVulpe <cosmin98vulpe@gmail.com>
2023-11-26 16:35:13 +0400

    slight refactor ReviewServiceUnitTest with MapStruct

--- src/test/java/com/jdm/legends/dealership/cars/unit/ReviewServiceUnitTest.java
+++ src/test/java/com/jdm/legends/dealership/cars/unit/ReviewServiceUnitTest.java
@@ -1,10 +1,11 @@
 package com.jdm.legends.dealership.cars.unit;
 
 
+import com.jdm.legends.dealership.cars.controller.dto.ReviewDTO;
 import com.jdm.legends.dealership.cars.controller.dto.ReviewRequest;
 import com.jdm.legends.dealership.cars.service.ReviewService;
 import com.jdm.legends.dealership.cars.service.entity.Review;
-import com.jdm.legends.dealership.cars.service.mapping.Mapper;
+import com.jdm.legends.dealership.cars.service.mapper.ReviewMapper;
 import com.jdm.legends.dealership.cars.service.repository.ReviewRepository;
 import com.jdm.legends.dealership.cars.utils.TestDummy;
 import org.junit.jupiter.api.Test;
@@ -23,7 +24,11 @@ import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-class ReviewServiceUnitTest implements Mapper<ReviewRequest, Review> {
+class ReviewServiceUnitTest {
+
+    private static final String TITLE = "good";
+    private static final String DESCRIPTION = "i love it";
+    private static final int STAR_RATING = 5;
 
     @Mock
     private ReviewRepository reviewRepository;
@@ -33,31 +38,26 @@ class ReviewServiceUnitTest implements Mapper<ReviewRequest, Review> {
 
     @Test
     void shouldGetAllReviewSuccessfully() {
-        List<Review> reviewList = List.of(new Review(), new Review());
-        when(reviewRepository.getRecentReviews()).thenReturn(reviewList);
+        List<ReviewDTO> reviewList = List.of(new ReviewDTO(TITLE, DESCRIPTION, STAR_RATING), new ReviewDTO(TITLE, DESCRIPTION, STAR_RATING));
+        when(reviewRepository.getRecentReviews()).thenReturn(
+                List.of( Review.builder().title(TITLE).starRating(STAR_RATING).description(DESCRIPTION).build()
+                        , Review.builder().title(TITLE).starRating(STAR_RATING).description(DESCRIPTION).build()));
 
-        List<Review> recentReviews = reviewService.getRecentReviews();
+        List<ReviewDTO> recentReviews = reviewService.getRecentReviews();
         assertThat(recentReviews).isEqualTo(reviewList);
     }
 
     @Test
     void shouldAddReviewSuccessfullyIntoDB() {
         ReviewRequest reviewRequest = TestDummy.buildReviewRequest();
-        Review review = map(reviewRequest);
+        Review review = ReviewMapper.INSTANCE.reviewRequestToReviewEntity(reviewRequest);
         when(reviewRepository.save(any())).thenReturn(review);
 
-        ResponseEntity<Review> reviewResponseEntity = reviewService.addReview(reviewRequest);
+        ResponseEntity<ReviewDTO> reviewResponseEntity = reviewService.addReview(reviewRequest);
 
         verify(reviewRepository).save(review);
         assertThat(reviewResponseEntity.getStatusCode()).isEqualTo(HttpStatus.CREATED);
+        assertThat(reviewResponseEntity.getBody()).isNotNull();
     }
 
-    @Override
-    public Review map(ReviewRequest source) {
-        return Review.builder()
-                .title(source.title())
-                .description(source.description())
-                .starRating(source.starRating())
-                .build();
-    }
 }
