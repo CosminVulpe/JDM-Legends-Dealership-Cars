e30297ebb568b5992db25104d263bc1ccefd9ee1
CosminVulpe <cosmin98vulpe@gmail.com>
2023-11-26 17:19:18 +0400

    slight refactoring ReviewRepositoryIT with MapStruct

--- src/test/java/com/jdm/legends/dealership/cars/integration/repositories/ReviewRepositoryIT.java
+++ src/test/java/com/jdm/legends/dealership/cars/integration/repositories/ReviewRepositoryIT.java
@@ -1,8 +1,7 @@
 package com.jdm.legends.dealership.cars.integration.repositories;
 
-import com.jdm.legends.dealership.cars.controller.dto.ReviewRequest;
 import com.jdm.legends.dealership.cars.service.entity.Review;
-import com.jdm.legends.dealership.cars.service.mapping.Mapper;
+import com.jdm.legends.dealership.cars.service.mapper.ReviewMapper;
 import com.jdm.legends.dealership.cars.service.repository.ReviewRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,25 +18,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test-in-memory")
 @Transactional
-class ReviewRepositoryIT implements Mapper<ReviewRequest,Review> {
+class ReviewRepositoryIT {
 
     @Autowired
     private ReviewRepository reviewRepository;
 
     @Test
     void shouldGetRecentReviews() {
-        IntStream.range(0, 7).forEach(item-> reviewRepository.save(map(buildReviewRequest())));
+        IntStream.range(0, 7).forEach(item-> reviewRepository.save(ReviewMapper.INSTANCE.reviewRequestToReviewEntity(buildReviewRequest())));
 
         List<Review> recentReviews = reviewRepository.getRecentReviews();
         assertThat(recentReviews).hasSize(5);
-    }
-
-    @Override
-    public Review map(ReviewRequest source) {
-        return Review.builder()
-                .description(source.description())
-                .title(source.title())
-                .starRating(source.starRating())
-                .build();
     }
 }
