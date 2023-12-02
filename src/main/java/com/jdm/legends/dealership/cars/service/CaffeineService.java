807daeace13bf7a6b907889ee02c8d488a451a87
CosminVulpe <cosmin98vulpe@gmail.com>
2023-12-02 17:31:21 +0400

    Slight refactoring

--- src/main/java/com/jdm/legends/dealership/cars/service/CaffeineService.java
+++ src/main/java/com/jdm/legends/dealership/cars/service/CaffeineService.java
@@ -4,9 +4,7 @@ import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.jdm.legends.dealership.cars.controller.dto.CountryResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Optional;
@@ -18,7 +16,6 @@ import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 public class CaffeineService {
     private final static Cache<String, CountryResponse> cache = Caffeine.newBuilder().expireAfterWrite(24, TimeUnit.HOURS).build();
 
-
     public void saveInCache(String key, CountryResponse response) {
         cache.put(key, response);
     }
@@ -26,7 +23,6 @@ public class CaffeineService {
     public Optional<CountryResponse> getCountryResponseCache(String key) {
         return Optional.ofNullable(cache.getIfPresent(key));
     }
-
 
     @Slf4j
     @ResponseStatus(code = INTERNAL_SERVER_ERROR)
