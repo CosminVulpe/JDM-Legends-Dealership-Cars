c517722fcef7249034f776b02165d007c8bf7fe6
CosminVulpe <cosmin98vulpe@gmail.com>
2024-01-13 17:48:39 +0400

    Add max age CORS

--- src/main/java/com/jdm/legends/dealership/cars/cors/ConfigGlobalCors.java
+++ src/main/java/com/jdm/legends/dealership/cars/cors/ConfigGlobalCors.java
@@ -1,16 +1,24 @@
 package com.jdm.legends.dealership.cars.cors;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class ConfigGlobalCors implements WebMvcConfigurer {
+    private final int maxAge;
+
+    public ConfigGlobalCors(@Value("${cors.max-age-time}") int maxAge) {
+        this.maxAge = maxAge;
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOriginPatterns("http://localhost:3000/")
-                .allowedHeaders("*");
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(maxAge);
     }
 }
