b7935d10b73d50293814991382be574e42c1ce6e
CosminVulpe <cosmin98vulpe@gmail.com>
2024-02-03 13:04:46 +0400

    Slight Refactor CORS Policy

--- src/main/java/com/jdm/legends/dealership/cars/cors/ConfigGlobalCors.java
+++ src/main/java/com/jdm/legends/dealership/cars/cors/ConfigGlobalCors.java
@@ -1,24 +1,34 @@
 package com.jdm.legends.dealership.cars.cors;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
-public class ConfigGlobalCors implements WebMvcConfigurer {
+public class ConfigGlobalCors {
     private final int maxAge;
 
     public ConfigGlobalCors(@Value("${cors.max-age-time}") int maxAge) {
         this.maxAge = maxAge;
     }
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:3000/")
-                .allowedHeaders("*")
-                .allowCredentials(true)
-                .maxAge(maxAge);
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:3000");
+        corsConfiguration.setAllowedMethods(List.of("*"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        corsConfiguration.setAllowCredentials(Boolean.TRUE);
+        corsConfiguration.setMaxAge((long) maxAge);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
     }
 }
