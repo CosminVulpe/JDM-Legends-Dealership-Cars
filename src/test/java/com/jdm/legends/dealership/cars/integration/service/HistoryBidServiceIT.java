542fac8316375909c30ba2f024d12606bd865522
CosminVulpe <cosmin98vulpe@gmail.com>
2024-03-02 19:33:36 +0400

    Add test HistoryBidServiceIT

--- src/test/java/com/jdm/legends/dealership/cars/integration/service/HistoryBidServiceIT.java
+++ src/test/java/com/jdm/legends/dealership/cars/integration/service/HistoryBidServiceIT.java
@@ -2,9 +2,12 @@ package com.jdm.legends.dealership.cars.integration.service;
 
 import com.jdm.legends.dealership.cars.service.HistoryBidService;
 import com.jdm.legends.dealership.cars.service.entity.Car;
+import com.jdm.legends.dealership.cars.service.entity.HistoryBid;
 import com.jdm.legends.dealership.cars.service.repository.CarRepository;
+import com.jdm.legends.dealership.cars.service.repository.CustomerRepo;
 import com.jdm.legends.dealership.cars.service.repository.HistoryBidRepository;
 import com.jdm.legends.dealership.cars.service.repository.TemporaryCustomerRepo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,6 +19,7 @@ import static com.jdm.legends.dealership.cars.utils.TestDummy.buildCarRequest;
 import static com.jdm.legends.dealership.cars.utils.TestDummy.getHistoryBidTempCustomerMock;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.verify;
 
 @SpringBootTest
@@ -35,15 +39,33 @@ class HistoryBidServiceIT {
     @MockBean
     private TemporaryCustomerRepo temporaryCustomerRepo;
 
+    @MockBean
+    private CustomerRepo customerRepo;
+
+    @BeforeEach
+    void init() {
+        carRepository.save(buildCarRequest());
+    }
+
     @Test
     void bidPerCarSuccessfully() {
-        Car car = carRepository.save(buildCarRequest());
-
+        Car car = carRepository.findAll().get(0);
         historyBidService.bid(car.getId(), getHistoryBidTempCustomerMock());
 
         assertThat(historyBidRepository.findAll()).hasSize(1);
-        assertThat(historyBidRepository.findAll()).isNotEmpty();
+        assertThat(historyBidRepository.findAll()).hasAtLeastOneElementOfType(HistoryBid.class);
 
         verify(temporaryCustomerRepo).saveTempUser(any(), any());
     }
+
+    @Test
+    void bidPerCarSuccessfullyCustomer() {
+        Car car = carRepository.findAll().get(0);
+        doNothing().when(customerRepo).assignCustomerIdToHistoryBid(any(), any());
+
+        historyBidService.bidCustomer(car.getId(), getHistoryBidTempCustomerMock("john@gmail.com").historyBidRequest());
+
+        assertThat(historyBidRepository.findAll()).hasSize(1);
+        assertThat(historyBidRepository.findAll()).hasAtLeastOneElementOfType(HistoryBid.class);
+   }
 }
